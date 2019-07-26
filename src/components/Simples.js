80cec27b61101278a90c6d7964b39f60c10a7e74
Anderson <mfderson@gmail.com>
2019-07-25 21:40:57 -0300

    22. Criando estilo

--- src/components/Simples.js
+++ src/components/Simples.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import { Text } from 'react-native'
+import Padrao from '../style/Padrao'
+
+export default props => <Text style={Padrao.ex}>Arrow 1: {props.texto}</Text>
+
+// ERROR!!!
+// export default props => 
+//   <Text>{props.texto}</Text>
+//   <Text>{props.texto}</Text>
 
 // export default function(props) {
 //   return <Text>{props.texto}</Text>
@@ -13,11 +21,6 @@ import { Text } from 'react-native'
 // Correto
 // export default props => <Text>{props.texto}</Text>
 
-// ERROR!!!
-// export default props => 
-//   <Text>{props.texto}</Text>
-//   <Text>{props.texto}</Text>
-
 // Correto
 // export default props => 
 //   <View>
@@ -25,7 +28,8 @@ import { Text } from 'react-native'
 //     <Text>{props.texto}</Text>
 //   </View>
 
-export default props => [
-  <Text key={1}>Arrow 1: {props.texto}</Text>,
-  <Text key={2}>Arrow 2: {props.texto}</Text>
-]
+// Correto
+// export default props => [
+//   <Text key={1}>Arrow 1: {props.texto}</Text>,
+//   <Text key={2}>Arrow 2: {props.texto}</Text>
+// ]
